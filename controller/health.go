4df09d428bb9b0aa089a4da52f0a0fed13d11f62
Cedric Shih <cedric.shih@gmail.com>
2022-07-25 21:26:03 +0800

    enforce auth on health check

--- controller/health.go
+++ controller/health.go
@@ -14,7 +14,7 @@ type HealthController struct{}
 func (c *HealthController) Check(w http.ResponseWriter, r *http.Request) {
 	auth := config.Get().GetString("healthcheck.auth")
 	if auth == "" {
-		http.Error(w, "Missing healthcheck authorization", 401)
+		http.Error(w, "Missing health check authorization", 401)
 		return
 	}
 	if auth != r.Header.Get("Authorization") {
