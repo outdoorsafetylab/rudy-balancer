570ad5e066394e0f206febf137c306b599dadcc8
Cedric Shih <cedric.shih@appier.com>
2025-03-26 19:31:52 +0800

    chore: add referer and method to http logs for looker studio

--- controller/file.go
+++ controller/file.go
@@ -29,23 +29,24 @@ func (c *FileController) Download(w http.ResponseWriter, r *http.Request) {
 	dao := &dao.FileDao{SiteDao: dao.SiteDao{Context: r.Context()}}
 	sources, err := dao.GetSources(c.File)
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	if sources == nil || len(sources) <= 0 {
 		log.Warningf("No available sources: %s", c.File)
-		http.Error(w, fmt.Sprintf("No available sources: %s", c.File), 502)
+		http.Error(w, fmt.Sprintf("No available sources: %s", c.File), http.StatusBadGateway)
 		return
 	}
-	rand.Seed(time.Now().UnixNano())
 	src := sources[rand.Intn(len(sources))]
-	http.Redirect(w, r, src.URL, 302)
+	http.Redirect(w, r, src.URL, http.StatusFound)
 	stop := time.Now()
 	if r.Method != "GET" {
 		return
 	}
 	go func() {
 		fields := []zapcore.Field{
+			zap.String("Method", r.Method),
+			zap.String("Referer", r.Referer()),
 			zap.String("UserAgent", r.UserAgent()),
 			zap.String("SiteName", src.SiteName),
 			zap.String("File", src.File),
@@ -85,7 +86,7 @@ func (c *FileController) List(w http.ResponseWriter, r *http.Request) {
 	dao := &dao.FileDao{SiteDao: dao.SiteDao{Context: r.Context()}}
 	files, err := dao.Files()
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	writeJSON(w, r, files)
